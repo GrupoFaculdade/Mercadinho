fbbcb2d0aeec858bdd8d70a0e12beccf97c1110d
DougSantos <douglasaac@gmail.com>
2013-10-20 15:56:47 -0300

    13:56 - 20/10

--- Mercadinho/src/mercadinho/Dados/DadosFornecedor.java
+++ Mercadinho/src/mercadinho/Dados/DadosFornecedor.java
@@ -5,11 +5,13 @@
  */
 package mercadinho.Dados;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import mercadinho.ClassesBasicas.CamadaBanco;
 import mercadinho.ClassesBasicas.Fornecedor;
-import mercadinho.ClassesBasicas.FornecedorExeception;
+import mercadinho.ClassesBasicas.FornecedorException;
 
 /**
  *
@@ -17,26 +19,83 @@ import mercadinho.ClassesBasicas.FornecedorExeception;
  */
 public class DadosFornecedor extends CamadaBanco {
 
+    private Statement callBd;
+    private String sqlQuery;
+    private ResultSet getResult;
 
-    public void cadastrarFornecedor(Fornecedor f) throws FornecedorExeception {
+    public void cadastrarFornecedor(Fornecedor forn) throws FornecedorException {
 
         try {
-            Statement conex = conectar();
-            String sql = "INSERT INTO fornecedores VALUES";
-            sql += "('" + f.getCodigoFornecedor() + "','" + f.getNome() + "')";
-            conex.executeQuery(sql);
+            callBd = conectar();
+            sqlQuery = "INSERT INTO fornecedores VALUES"
+            + "('" + forn.getCodigoFornecedor() + "','" + forn.getNome() + "')";
+            callBd.execute(sqlQuery);
 
-        } catch (Exception ex) {
-
-            throw new FornecedorExeception(ex.getMessage());
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new FornecedorException(ex.getMessage());
         } finally {
             try {
                 desconectar();
             } catch (SQLException ex) {
-                throw new FornecedorExeception(ex.getMessage());
+                throw new FornecedorException(ex.getMessage());
             }
 
         }
 
+    }
+    
+     public void removerCliente(String codForn) throws FornecedorException {
+        try {
+            this.callBd = conectar();
+            sqlQuery = "DELETE FROM fornecedores WHERE codFornecedor = " + codForn + ";";
+            callBd.execute(sqlQuery);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new FornecedorException(ex.getMessage());
+        } finally {
+            try {
+                desconectar();
+            } catch (SQLException ex) {
+                throw new FornecedorException(ex.getMessage());
+            }
+        }
+    }
+    
+    public void alterarFornecedor(Fornecedor forn) throws FornecedorException {
+        try {
+            callBd = conectar();
+            sqlQuery = "UPDATE fornecedores SET nome = '"+forn.getNome()+"' WHERE codFornecedor = "+forn.getCodigoFornecedor()+" ;"; 
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new FornecedorException(ex.getMessage());
+        } finally {
+            try {
+                desconectar();
+            } catch (SQLException ex) {
+                throw new FornecedorException(ex.getMessage());
+            }
+        }
+    }
+    
+    public ArrayList<Fornecedor> Listar(String filtro) throws FornecedorException {
+        try {
+            ArrayList<Fornecedor> listagem = new ArrayList<>();
+            if (filtro.equals("")) {
+                sqlQuery = "SELECT * FROM fornecedores;";
+            } else {
+                sqlQuery = "SELECT * FROM fornecedores WHERE nome LIKE '%" + filtro + "%';";
+            }
+
+            callBd = conectar();
+            getResult = callBd.executeQuery(sqlQuery);
+            while (getResult.next()) {
+                Fornecedor forn = new Fornecedor();
+                forn.setNome(getResult.getString("nome"));
+                forn.setCodigoFornecedor(getResult.getString("codFornecedor"));
+                listagem.add(forn);
+            }
+            return listagem;
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new FornecedorException(e.getMessage());
+        }
     }
 }
