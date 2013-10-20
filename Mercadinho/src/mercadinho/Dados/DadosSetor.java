c821dd83f110c1e7ee3ae6ecfa743e6791ccaf54
Hidanebr <klebersant@live.com>
2013-10-20 16:36:11 -0200

    20/10/2013 ás 16h57min

--- Mercadinho/src/mercadinho/Dados/DadosSetor.java
+++ Mercadinho/src/mercadinho/Dados/DadosSetor.java
@@ -5,10 +5,73 @@
  */
 package mercadinho.Dados;
 
+import java.sql.Statement;
+import mercadinho.ClassesBasicas.CamadaBanco;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import mercadinho.ClassesBasicas.SetorProdutoException;
+import mercadinho.ClassesBasicas.SetorProdutos;
+
 /**
  *
  * @author NeGo
  */
-public class DadosSetor {
+public class DadosSetor  extends CamadaBanco {
     
+    private Statement callBd;
+    private String sqlQuery;
+   private ResultSet getResultSet;
+   
+   
+   public void  cadastrarSetor (SetorProdutos sec) throws SetorProdutoException{
+       try{
+           callBd =conectar();
+           sqlQuery="INSERT INTO SetorProdutos VALUES";
+            sqlQuery += "('"+sec.getCodigoSetor()+"', '"+sec.getNome()+"')";
+            callBd.executeQuery(sqlQuery);
+       }catch(ClassNotFoundException | SQLException ex){
+           throw new SetorProdutoException(ex.getMessage());
+           
+       }finally{
+           try{
+               
+               desconectar();
+               
+           }catch(SQLException ex){
+               throw new SetorProdutoException(ex.getMessage());
+           }
+       }
+           
+         
+       
+   }
+   public void  removerSetor(SetorProdutos sec) throws SetorProdutoException{
+       try{
+           callBd =conectar();
+           sqlQuery="DELETE FROM SetorProdutos WHERE SetorProdutos = "+sec.getCodigoSetor()+",";
+       }catch(ClassNotFoundException  | SQLException ex){
+           throw new SetorProdutoException(ex.getMessage());
+       }finally{
+           try{
+               desconectar();
+           }catch(SQLException ex){
+               throw new SetorProdutoException(ex.getMessage());
+           }
+       }
+   }
+    public void alterarSetor(SetorProdutos sec) throws SetorProdutoException{
+        try{
+            callBd=conectar();
+            sqlQuery="PDATE SetorProdutos set nome ="+sec.getNome()+"',codigoSetor"+sec.getCodigoSetor()+"',";
+        }catch(ClassNotFoundException | SQLException ex){
+            throw new SecurityException(ex.getMessage());
+            
+        }finally{
+            try{
+                desconectar();
+            }catch(SQLException ex){
+                throw new SetorProdutoException(ex.getMessage());
+            }
+        }
+    }
 }
